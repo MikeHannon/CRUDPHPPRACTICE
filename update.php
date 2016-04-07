dc253c9e228ff764f29f58487f92df131246df0e
Mike Hannon <mjhannon@gmail.com>
2016-04-07 14:52:50 -0700

    updated slightly

--- update.php
+++ update.php
@@ -1,8 +1,10 @@
 <?php
 session_start();
 // You goal is to fix this!
-var_dump($_POST);
-die("This update function needs work - fix it!");
 
-header("Location:users_index.php");
+var_dump($_POST['user_id']);
+var_dump($_SESSION['users'][$_POST['user_id']]);
+// die("This update function needs work - fix it!");
+
+// header("Location:users_index.php");
 ?>
